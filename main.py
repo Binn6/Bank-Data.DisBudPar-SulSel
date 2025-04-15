499e83d89d70f6e6a711bd9f81f8477dbb641429
Binn6 <mochsabilabyan12@gmail.com>
2025-04-15 14:05:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-# Set page config harus menjadi perintah Streamlit pertama
-st.set_page_config(page_title="Aplikasi Input Data Pariwisata", layout="centered")
-
-# Langsung arahkan ke halaman login
-st.switch_page("pages/0_login.py")
+st.set_page_config(page_title="Redirecting...", layout="centered")
+st.write("⏳ Mengarahkan ke halaman login...")
+st.experimental_rerun()
